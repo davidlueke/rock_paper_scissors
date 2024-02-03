800789f2c22b44fe99f5b67d5bec1fab54cadab5
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-03 13:22:41 +0100

    Add pseudocode for player choice

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Computer Choice
 // Create function
 // Generate a random number
-// Store value in variable
+// Store value in variable -> not needed
 // Return variable
 
 function getComputerChoice() {                              //Function provides a number as choice for the computer
     return Math.floor(Math.random() * 3) + 1;               //Generates a random number between 1 and 3
 }
+
+//Player choice
+//Create a function
+//Prompt the user for a choice of Rock, Paper, Scissors
+//Make sure all forms writing styles are accepted
+//An Error message like "No valid choice" wil be presented if the choice is not valid
+//Convert the input to representing number
+
+
+
