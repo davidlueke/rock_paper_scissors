3d39daead9f037d2d57af57c9825114357e1241c
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-03 13:17:41 +0100

    Add a function called getComputerChoice
    Function generates a random number between 1 and 3
    which will be used as the choice of the computer
    in the game.

--- script.js
+++ script.js
@@ -4,3 +4,6 @@
 // Store value in variable
 // Return variable
 
+function getComputerChoice() {                              //Function provides a number as choice for the computer
+    return Math.floor(Math.random() * 3) + 1;               //Generates a random number between 1 and 3
+}
