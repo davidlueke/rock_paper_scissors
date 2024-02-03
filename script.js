2562bc96bf8f17fef87efce863fab2dd2197927b
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-03 13:44:32 +0100

    Add getPlayerChoice function and remove pseudocode
    getPlayerChoice asks the user for their choice of
    rock, paper or scissors and checks if the option
    is valid. afterward it returns the representing numeric
    value of those options

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// Computer Choice
-// Create function
-// Generate a random number
-// Store value in variable -> not needed
-// Return variable
-
-function getComputerChoice() {                              //Function provides a number as choice for the computer
-    return Math.floor(Math.random() * 3) + 1;               //Generates a random number between 1 and 3
+function getComputerChoice() {                                                  //Function provides a number as choice for the computer
+    return Math.floor(Math.random() * 3) + 1;                                   //Generates a random number between 1 and 3
 }
 
-//Player choice
-//Create a function
-//Prompt the user for a choice of Rock, Paper, Scissors
-//Make sure all forms writing styles are accepted
-//An Error message like "No valid choice" wil be presented if the choice is not valid
-//Convert the input to representing number
+function getPlayerChoice() {
 
-
-
+    let choice = prompt("Make your choice! Rock, paper or scissors?")           //Player got to choose what to enter
+    choice = choice.toLowerCase()                                               //Converts input of the player to lowercase to accept different writing style
+    
+    if (choice !== "rock" && choice !== "paper" && choice !== "scissors"){      //Checks if the player input is a valid input
+        console.log("No valid option")                                          //Let's the player know that the input is invalid
+    }else if (choice == "rock"){                                                            
+        return 1;
+    }else if (choice == "paper"){
+        return 2;
+    }else if (choice == "scissors"){                                            //Each "else if" converts the player choice to a number to compare it with choice of the computer
+        return 3;
+    }
+}
