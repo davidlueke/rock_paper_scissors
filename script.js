2a49d2d1ba4393ab80ec3122ffc1c9fb7b2281f8
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-12 11:42:20 +0100

    Add events to buttons and remove unnecessary code
    getPlayerChoices is no longer needed because of the
    button function calls.
    
    Deciding if clause removed because of a new winning
    condition.

--- script.js
+++ script.js
@@ -2,22 +2,6 @@ function getComputerChoice() {                                                  
     return Math.floor(Math.random() * 3) + 1;                                           //Generates a random number between 1 and 3
 }
 
-function getPlayerChoice() {
-
-    let choice = prompt("Make your choice! Rock, paper or scissors?")                   //Player got to choose what to enter
-    choice = choice.toLowerCase()                                                       //Converts input of the player to lowercase to accept different writing style
-    
-    if (choice !== "rock" && choice !== "paper" && choice !== "scissors"){              //Checks if the player input is a valid input
-        console.log("No valid option")                                                  //Let's the player know that the input is invalid
-    }else if (choice == "rock"){                                                            
-        return 1;
-    }else if (choice == "paper"){
-        return 2;
-    }else if (choice == "scissors"){                                                    //Each "else if" converts the player choice to a number to compare it with choice of the computer
-        return 3;
-    }
-}
-
 let countComputer = 0
 let countPlayer = 0
 let result = ""
@@ -68,12 +52,10 @@ function playRound(playerSelection, computerSelection){                         
     }
 }
 
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
 
-
-if (countPlayer > countComputer){                                                       //if statement compares the overall counter an decides who has won
-    console.log(`With ${countPlayer}:${countComputer} you are the winner!`)             //Based on the comparison the corresponding message is printed
-}else if (countPlayer < countComputer){
-    console.log(`With ${countPlayer}:${countComputer} you have lost!`)
-}else{
-    console.log(`With ${countPlayer}:${countComputer} no one has won! It's a draw!`)
-}
+rock.addEventListener("click", () => playRound(1, getComputerChoice));
+paper.addEventListener("click", () => playRound(2, getComputerChoice));
+scissors.addEventListener("click", () => playRound(3, getComputerChoice));
