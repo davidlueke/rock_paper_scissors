474168074605bc67990935842e5d9425ca8d1171
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-12 13:59:57 +0100

    Add winning message, simplify playRound, add functions
    Add function endGame which determines if someone has won, and removes
    the eventsListeners from the buttons
    
    Add functions handleRock/Paper/Scissors to get an functions
    without a parameter to be able to remove the eventsListeners by using
    the removeEventListeners method

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let countComputer = 0
-let countPlayer = 0
-
 function getComputerChoice() {                                                          //Function provides a number as choice for the computer
     return Math.floor(Math.random() * 3) + 1;                                           //Generates a random number between 1 and 3
 }
@@ -10,54 +7,75 @@ function updateScore() {
 
 function playRound(playerSelection, computerSelection){                                 //Function compares playerSelection and computerSelection and decides if it is a draw or if someone wins
     
+    
     if (playerSelection == computerSelection){                                          //If both numbers are equal "Its a draw!" is printed and the count variables are not incremented
         
         updateScore()
+        endGame()
 
-    }else if (playerSelection == 1 && computerSelection == 2) {                         //Every following "else if" compares choices:
+    }else if ((playerSelection == 1 && computerSelection == 2) ||
+            (playerSelection == 2 && computerSelection == 3) ||
+            (playerSelection == 3 && computerSelection == 1)) {                         
                                                                                 
-        countComputer++                                                                 //increment the counter of the winner                                                                                         
-        updateScore()                                   
+        countComputer++                                                                                       
+        updateScore()  
+        endGame()                                 
 
-    }else if (playerSelection == 1 && computerSelection == 3) {
-
+    }else if ((playerSelection == 1 && computerSelection == 3) ||
+            (playerSelection == 2 && computerSelection == 1) ||
+            (playerSelection == 3 && computerSelection == 2)) {
         countPlayer++
         updateScore()
+        endGame()
+    }
+}
+function endGame(){
+    if (countPlayer == 5 || countComputer == 5){
 
-    }else if (playerSelection == 2 && computerSelection == 1) {
-
-        countPlayer++
-        updateScore()
-
-    }else if (playerSelection == 2 && computerSelection == 3) {
-
-        countComputer++
-        updateScore() 
-
-    }else if (playerSelection == 3 && computerSelection == 1) {
-
-        countComputer++
-        updateScore()
-
-    }else if (playerSelection == 3 && computerSelection == 2) {
-
-        countPlayer++
-        updateScore()
+        if (countPlayer == 5){
+            message.appendChild(playerWins);
+            
+        }else{
+            message.appendChild(computerWins);
+            
+        }
+        rock.removeEventListener("click", handleRock);
+        paper.removeEventListener("click", handlePaper);
+        scissors.removeEventListener("click", handleScissors);
     }
 }
 
+function handleRock(){
+        playRound(1, getComputerChoice());
+}
 
-let points = document.createElement("p")
-let score = document.querySelector("#score")
+function handlePaper(){
+    playRound(2, getComputerChoice());
+}
 
+function handleScissors(){
+    playRound(3, getComputerChoice());
+}
+
+let countComputer = 0
+let countPlayer = 0
+
+let points = document.createElement("p");
 points.textContent = `${countPlayer}:${countComputer}`;
+let score = document.querySelector("#score");
+score.appendChild(points);
 
-score.appendChild(points)
+let message = document.querySelector("#message");
+let playerWins = document.createElement("p");
+let computerWins = document.createElement("p");
+
+playerWins.textContent ="You win!";
+computerWins.textContent ="You lose!";
 
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
-rock.addEventListener("click", () => playRound(1, getComputerChoice()));
-paper.addEventListener("click", () => playRound(2, getComputerChoice()));
-scissors.addEventListener("click", () => playRound(3, getComputerChoice()));
+rock.addEventListener("click", handleRock);
+paper.addEventListener("click", handlePaper);
+scissors.addEventListener("click", handleScissors);
